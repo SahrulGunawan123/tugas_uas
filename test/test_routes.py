108d3e44d87c847a8ffa58c13bcda5134f60e44b
affandial <affandi@datasintesa.id>
2023-12-21 17:26:41 +0700

    Fix Routes

--- test/test_routes.py
+++ test/test_routes.py
@@ -17,5 +17,18 @@ def test_product_api(client):
 
 # post new cart
 def test_post_cart(client):
-    pass
+    # Define a sample data payload for creating a new cart
+    cart_data = {
+        "coupon_code": "DISCOUNT123",
+        "shipping_fee": 5.0,
+        "cart_items": [
+            {"product_id": 1, "qty": 2},
+            {"product_id": 3, "qty": 1}
+        ]
+    }
 
+    # Make a POST request to create a new cart
+    response = client.post("/api/cart", json=cart_data)
+    
+    # Check that the response status code is 200 (OK)
+    assert response.status_code==200
